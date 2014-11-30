a83cbb516c2e6a969116990dda07929ccc8332c8
Jonas Schiegl <teh.b4k4@gmail.com>
2014-11-30 13:25:45 +0000

    initial working line renderer

--- Assets/Unitility/Core/LineGeometryGenerator.cs
+++ Assets/Unitility/Core/LineGeometryGenerator.cs
@@ -1,17 +1,30 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
+using NUnit.Framework.Constraints;
 using UnityEngine;
 
 namespace Assets.Unitility.Core
 {
     public struct LineData
     {
-        public Vector3[] Points;
         public float Alpha;
-        public float Width;
-        public float CapLength;
+        public CornerStyle Corners;
+        public float CornerRadius;
+        public LineCapType StartCap;
+        public LineCapType EndCap;
+        public float StartCapSize;
+        public float EndCapSize;
         public Color Color;
-        public LineCapType Cap;
+        public Vector3[] Points;
+        public float Width;
+    }
+
+    public enum CornerStyle
+    {
+        Sharp,
+        Rounded
     }
 
     public enum LineCapType
@@ -22,26 +35,52 @@ namespace Assets.Unitility.Core
         Round
     }
 
-    [RequireComponent(typeof(MeshFilter))]
-    [RequireComponent(typeof(MeshRenderer))]
+    [RequireComponent(typeof (MeshFilter))]
+    [RequireComponent(typeof (MeshRenderer))]
     public class LineGeometryGenerator : MonoBehaviour
     {
-        private List<LineData> _lines;
         private MeshFilter _filter;
+        private List<LineData> _lines;
         private MeshRenderer _renderer;
- 
-        public LineCapType LineCap;
-        public float CapLength;
         public string SortingLayer;
-
         public float LineWidth;
-
         // Use this for initialization
-        void Start ()
+        private void Start()
         {
             _filter = GetComponent<MeshFilter>();
             _filter.mesh = new Mesh();
             _renderer = GetComponent<MeshRenderer>();
+            _renderer.material.mainTexture.wrapMode = TextureWrapMode.Repeat;
+
+            StartCoroutine(MeshUpdate());
+        }
+
+        private IEnumerator MeshUpdate()
+        {
+            while (true)
+            {
+                SetLinesFromChildren();
+                for (int i = 0; i < 1; i++)
+                {
+                    yield return null;
+                }
+            }
+        }
+
+        private void SetLinesFromChildren()
+        {
+            var test = new LineData
+            {
+                Points = Enumerable.Range(0, transform.childCount).Select(i => transform.GetChild(i).position).ToArray(),
+                Width = LineWidth,
+                CornerRadius = 0.6f * LineWidth,
+                Corners = CornerStyle.Rounded,
+                StartCap = LineCapType.Arrow,
+                EndCap = LineCapType.Arrow,
+                EndCapSize = 1.5f * LineWidth,
+                StartCapSize = LineWidth
+            };
+            SetLines(new[] { test });
         }
 
         public void SetLines(IEnumerable<LineData> lines)
@@ -50,44 +89,220 @@ namespace Assets.Unitility.Core
             RecalculateGeometry();
         }
 
-        private void RecalculateGeometry()
+        private void AddSegment(MeshProxy proxy, Vector3 p1, Vector3 p2, Vector3 t1, 
+                                Vector3? t2n = null)
         {
-            _renderer.sortingLayerName = SortingLayer;
+            var t2 = t2n ?? t1;
 
-            var vertices = new List<Vector3>();
-            var normals = new List<Vector3>();
-            var uvs = new List<Vector2>();
-            var triangles = new List<int>();
+            //Debug.DrawLine(p1 - t1, p1 - t1 + 0.1f * Vector3.up, Color.white, 12);
+            //yield return new WaitForSeconds(0.25f);
+            //Debug.DrawLine(p1 + t1, p1 + t1 + 0.1f * Vector3.up, Color.cyan, 11.75f);
+            //yield return new WaitForSeconds(0.25f);
+            //Debug.DrawLine(p2 - t2, p2 - t2 + 0.1f * Vector3.up, Color.black, 11.5f);
+            //yield return new WaitForSeconds(0.25f);
+            //Debug.DrawLine(p2 + t2, p2 + t2 + 0.1f * Vector3.up, Color.red, 11.25f);
+            //yield return new WaitForSeconds(0.25f);
+            proxy.AddQuad(
+                p1 - t1,
+                p1 + t1,
+                p2 - t2,
+                p2 + t2,
+                Vector2.zero,
+                Vector2.right,
+                Vector2.up,
+                Vector2.one);
+        }
 
-            foreach (var line in _lines)
+        private void AddCircleSegment(MeshProxy proxy, Vector3 center, Vector3 startPointer, float angle, int subdivisions, bool ccw = false)
+        {
+            var angleStep = angle / subdivisions;
+            var axis = ccw ? Vector3.down : Vector3.up;
+            for (int i = 0; i < subdivisions; i++)
             {
-                var edges = new Vector3[line.Points.Length-1];
-                for (int i = 0; i < line.Points.Length - 1; i++)
+                proxy.AddQuad(center, center + Quaternion.AngleAxis(i*angleStep, axis)*startPointer,
+                    center,
+                    center + Quaternion.AngleAxis((i + 1)*angleStep, axis)*startPointer, 0.5f*Vector2.right,
+                    Vector2.right, new Vector2(0.5f, 1), Vector2.one);
+            }
+        }
+
+        private void AddRingSegment(MeshProxy proxy, Vector3 center, Vector3 normal, Vector3 startPointer,
+            float innerRadius, float outerRadius, float angle)
+        {
+            var subdivisions = (int)(angle/8);
+            var angleStep = angle/subdivisions;
+
+            if (normal.y < 0)
+            {
+                center += (innerRadius+outerRadius)* startPointer;
+                startPointer *= -1;
+            }
+
+            for (int i = 0; i < subdivisions; i++)
+            {
+                if (normal.y > 0)
                 {
-                    edges[i] = line.Points[i + 1] - line.Points[i];
+                    var firstPointer = Quaternion.AngleAxis(i * angleStep, normal) * startPointer;
+                    var secondPointer = Quaternion.AngleAxis((i + 1) * angleStep, normal) * startPointer;
+                    proxy.AddQuad(center + outerRadius*firstPointer, center + innerRadius*firstPointer,
+                        center + outerRadius*secondPointer, center + innerRadius*secondPointer, Vector2.zero,
+                        Vector2.right, Vector2.up, Vector2.one);
                 }
-                Vector3 previousLocalRight;
-                for (int i = 0; i < line.Points.Length; i++)
+                else
                 {
-                    if (i == 0)
-                    {
-                        previousLocalRight = Vector3.Cross(edges[0], Vector3.up);
-                        continue;
-                    }
-                    var localRight = i < line.Points.Length - 1 
-                        ? Vector3.Cross(edges[i-1]+edges[i], Vector3.up)
-                        : Vector3.Cross(edges[i], Vector3.up);
-
-                    vertices.Add(line.Points[i-1]);
-
-                    previousLocalRight = localRight;
+                    var firstPointer = Quaternion.AngleAxis(i * angleStep, normal) * startPointer;
+                    var secondPointer = Quaternion.AngleAxis((i + 1) * angleStep, normal) * startPointer;
+                    proxy.AddQuad(center + innerRadius * firstPointer, center + outerRadius * firstPointer,
+                        center + innerRadius * secondPointer, center + outerRadius * secondPointer, Vector2.zero,
+                        Vector2.right, Vector2.up, Vector2.one);
                 }
             }
         }
 
+        private void AddCap(MeshProxy proxy, LineData line, Vector3 point, Vector3 direction, Vector3 t, bool endCap)
+        {
+            Vector3 end;
+            var capType = endCap ? line.EndCap : line.StartCap;
+            var capSize = endCap ? line.EndCapSize : line.StartCapSize;
+            switch (capType)
+            {
+                case LineCapType.None:
+                    break;
+                case LineCapType.Sharp:
+                    end = point + capSize * direction;
+                    proxy.AddQuad(point - t, point, end, point,
+                        Vector2.zero, 0.5f * Vector2.right, Vector2.up, new Vector2(0.5f, 1));
+                    proxy.AddQuad(point, point + t, point, end, 
+                        0.5f * Vector2.right, Vector2.right, new Vector2(0.5f, 1), Vector2.one);
+                    break;
+                case LineCapType.Arrow:
+                    end = point + capSize * direction;
+                    var center = point + 0.5f*capSize*direction;
+                    var tNormalized = t.normalized;
+                    proxy.AddQuad(point - t, point, point - capSize * tNormalized, center,
+                        Vector2.zero, 0.5f * Vector2.right, Vector2.up, new Vector2(0.5f, 1));
+                    proxy.AddQuad(point - capSize * tNormalized, center, end, center,
+                        Vector2.zero, 0.5f*Vector2.right, Vector2.up, new Vector2(0.5f, 1));
+                    proxy.AddQuad(center, end, center, point+capSize*tNormalized,
+                        0.5f * Vector2.right, Vector2.right, new Vector2(0.5f, 1), Vector2.one);
+                    proxy.AddQuad(point + t, point + capSize * tNormalized, point, center,
+                       Vector2.right, Vector2.one, 0.5f * Vector2.right, new Vector2(0.5f, 1));
+                    break;
+                case LineCapType.Round:
+                    AddCircleSegment(proxy, point, t, 180, 10, true);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException("type");
+            }
+        }
+
+        private void ExpandCorner(MeshProxy proxy, LineData line, Vector3 point, Vector3 previousEdge, Vector3 nextEdge, Vector3 segmentStart, bool firstSegment)
+        {
+            var w = line.Width / 2;
+            var r = Mathf.Max(w, line.CornerRadius);
+            var angle = Vector3.Angle(previousEdge, nextEdge);
+            var sin = Mathf.Sin(Mathf.Deg2Rad*angle/2);
+            var cos = Mathf.Cos(Mathf.Deg2Rad*angle/2);
+            var hypothenuse = r / cos;
+            var slide = hypothenuse * sin;
+
+            //Debug.DrawLine(point, point + 0.25f*Vector3.up, Color.red, 2);
+            //yield return new WaitForSeconds(.05f);
+            //Debug.DrawLine(point, point - previousEdge, Color.blue, 2);
+            //Debug.DrawLine(point, point + nextEdge, Color.cyan, 2);
+            //yield return new WaitForSeconds(.1f);
+
+            var normalizedPrevious = previousEdge.normalized;
+
+            var normalizedNext = nextEdge.normalized;
+            var initial = point - slide * normalizedPrevious;
+            var initialRight = Vector3.Cross(Vector3.up, normalizedPrevious);
+            //Debug.DrawLine(initial, initial + 0.25f * Vector3.up, Color.yellow, 2);
+            //Debug.DrawLine(initial, initial + w * initialRight, Color.green, 2);
+            //yield return new WaitForSeconds(.05f);
+
+            // add connection between previous and current corner
+            
+            AddSegment(proxy, segmentStart, initial, w * initialRight);
+            if (firstSegment)
+            {
+                AddCap(proxy, line, segmentStart, -normalizedPrevious, -w * initialRight, false);
+            }
+
+            switch (line.Corners)
+            {
+                case CornerStyle.Sharp:
+                    var medianLength = hypothenuse*w/r;
+                    var final = point + slide*normalizedNext;
+                    var finalRight = Vector3.Cross(Vector3.up, normalizedNext);
+
+                    var medianRight = (initialRight + finalRight).normalized;
+                    //Debug.DrawLine(final, final + 0.25f * Vector3.up, Color.green, 2);
+                    //Debug.DrawLine(final, final + w * finalRight, Color.yellow, 2);
+                    //yield return new WaitForSeconds(.05f);
+                    //Debug.DrawLine(point, point + 0.25f * Vector3.up, Color.black, 2);
+                    //Debug.DrawLine(point, point + medianLength * medianRight, Color.blue, 2);
+                    //yield return new WaitForSeconds(.1f);
+                    AddSegment(proxy, initial, point, w * initialRight, medianLength * medianRight);
+                    AddSegment(proxy, point, final, medianLength * medianRight, w * finalRight);
+                    break;
+                case CornerStyle.Rounded:
+                    var cornerAngle = angle;
+                    var center = initial + r*initialRight;
+                    var delta = r - w;
+                    AddRingSegment(proxy, center, Vector3.Cross(normalizedPrevious, normalizedNext).normalized,
+                        -initialRight, delta, delta + line.Width, cornerAngle);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private void RecalculateGeometry()
+        {
+            _renderer.sortingLayerName = SortingLayer;
+
+            var proxy = new MeshProxy(new Mesh());
+
+            foreach (var line in _lines)
+            {
+                var edges = new Vector3[line.Points.Length - 1];
+                for (var i = 0; i < line.Points.Length - 1; i++)
+                {
+                    edges[i] = line.Points[i + 1] - line.Points[i];
+                }
+                for (var i = 1; i < line.Points.Length; i++)
+                {
+                    if (i == line.Points.Length-1)
+                    {
+                        var start = 0.5f * (proxy.vertices[proxy.vertices.Count - 1] + proxy.vertices[proxy.vertices.Count - 2]);
+                        var t = Vector3.Cross(Vector3.up, edges[i - 1]).normalized*line.Width/2;
+                        AddSegment(proxy, start, line.Points[i], t);
+                        AddCap(proxy, line, line.Points[i], edges[i - 1].normalized,
+                           t , true);
+                    }
+                    else
+                    {
+                        ExpandCorner(proxy, line, line.Points[i], edges[i - 1], edges[i],
+                            i == 1
+                                ? line.Points[0]
+                                : 0.5f*
+                                  (proxy.vertices[proxy.vertices.Count - 2] +
+                                   proxy.vertices[proxy.vertices.Count - 1]), i == 1);
+                    }
+                }
+            }
+
+            proxy.Commit(_filter);
+        }
+
         // Update is called once per frame
-        void Update () {
-    
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SetLinesFromChildren();
+            }
         }
     }
 }
---
+++ Assets/Unitility/Core/MeshTools.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Assets.Unitility.Core
+{
+    public class MeshProxy
+    {
+        private readonly Mesh _mesh;
+
+        public MeshProxy(Mesh mesh)
+        {
+            _mesh = mesh;
+            vertices = new List<Vector3>();
+            uv = new List<Vector2>();
+            triangles = new List<int>();
+        }
+
+        public List<Vector3> vertices { get; set; }
+        public List<Vector2> uv { get; set; }
+        public List<int> triangles { get; set; }
+
+        //public void AddTri(Vector3 bottomLeft, Vector3 bottomRight, Vector3 top, Vector2 uv1, Vector2 uv2, Vector2 uv3)
+        //{
+        //    var offset = vertices.Count;
+        //    vertices.Add(bottomLeft);
+        //    vertices.Add(bottomRight);
+        //    vertices.Add(top);
+        //    uv.Add(uv1);
+        //    uv.Add(uv2);
+        //    uv.Add(uv3);
+        //    triangles.AddRange(new[] { offset, offset + 2, offset + 1 });
+        //}
+
+        public void AddQuad(Vector3 bottomLeft, Vector3 bottomRight, Vector3 topLeft, Vector3 topRight, Vector2 uv1, Vector2 uv2, Vector2 uv3,
+            Vector2 uv4)
+        {
+            var offset = vertices.Count;
+            vertices.Add(bottomLeft);
+            vertices.Add(bottomRight);
+            vertices.Add(topLeft);
+            vertices.Add(topRight);
+            uv.Add(uv1);
+            uv.Add(uv2);
+            uv.Add(uv3);
+            uv.Add(uv4);
+            triangles.AddRange(new[]{offset, offset+2, offset+3, offset, offset+3, offset+1});
+        }
+
+        public void Commit(MeshFilter filter)
+        {
+            _mesh.vertices = vertices.ToArray();
+            _mesh.uv = uv.ToArray();
+            //_mesh.normals = vertices.Select(v => Vector3.up).ToArray();
+            _mesh.triangles = triangles.ToArray();
+            _mesh.RecalculateBounds();
+            _mesh.RecalculateNormals();
+            filter.mesh = _mesh;
+            //_mesh.UploadMeshData(false);
+        }
+    }
+}
